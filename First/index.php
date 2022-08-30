b06c5325673fb73945c3309d89c0f2be951d9c9a
Btldjus <108800613+Btldjus@users.noreply.github.com>
2022-08-30 15:51:46 +0500

    To-do list

---
+++ First/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Список дел</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Список дел</h1>
+        <form action="/add.php" method="post">
+            <input type="text" name="task" id="task" placeholder="Нужно сделать..." class="form-control">
+            <button type="submit" name="sendTask" class="btn btn-success">Отправить</button>
+        </form>
+
+        <?php
+        require 'configDB.php';
+
+        echo '<ul>';
+        $query = $pdo->query('SELECT * FROM `tasks` ORDER BY `id` DESC');
+
+        while($row = $query->fetch(PDO::FETCH_OBJ)) {
+            echo '<li><a href="/delete.php?id='.$row->id.'"><button>Удалить</button></a><b class="txt">'.$row->task.'</b></li>';
+        }
+        echo '</ul>';
+
+        ?>
+
+    </div>
+</body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$db = mysql_connect(
+    "localhost",
+
+)
